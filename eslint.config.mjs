1be863887f691904dcc1e34915bdf061684cdbd3
Bruno-carneiro-da-silva <49591443+bruno-carneiro-da-silva@users.noreply.github.com>
2024-05-24 15:21:02 -0300

    :boom: fix: setting up some  rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,14 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
+import pluginJs from "@eslint/eslint-plugin";
 
-
-export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.node }},
-  pluginJs.configs.recommended,
-];
+export default {
+  files: ["**/*.js"],
+  languageOptions: {
+    sourceType: "commonjs",
+    globals: globals.node
+  },
+  ...pluginJs.configs.recommended,
+  rules: {
+    "no-unused-vars": ["error", {argsIgnorePattern: "next"}],
+  }
+};

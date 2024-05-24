8854a977a1f996992a9852749a55622b7d788910
Bruno-carneiro-da-silva <49591443+bruno-carneiro-da-silva@users.noreply.github.com>
2024-05-24 15:22:29 -0300

    :consrtuction: feat: handling the errors correctly

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('express-async-errors');
 
 const router = require('./routes');
 
@@ -7,4 +8,8 @@ const app = express();
 app.use(express.json());
 app.use(router);
 
+app.use((error, request, response, next) => {
+  console.log(error);
+  response.status(500).json({ error: 'Internal server error' });
+})
 app.listen(3000, () => console.log('Server is running on http://localhost:3000'))

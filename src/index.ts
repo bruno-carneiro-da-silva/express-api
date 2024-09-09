192a2758f75c04888bad94905e936daedcd2a485
Bruno / <49591443+bruno-carneiro-da-silva@users.noreply.github.com>
2024-09-09 14:43:15 -0300

    :boom: fix: vercel file

--- src/index.ts
+++ src/index.ts
@@ -23,18 +23,6 @@ const runServer = (port: number, server: http.Server) => {
   });
 };
 
+const serverPort: number = process.env.PORT ? parseInt(process.env.PORT) : 9000;
 const regularServer = http.createServer(app);
-if (process.env.NODE_ENV === "production") {
-  const options = {
-    key: fs.readFileSync(process.env.SSL_KEY as string),
-    cert: fs.readFileSync(process.env.SSL_CERT as string),
-  };
-  const secureServer = https.createServer(options, app);
-  runServer(80, regularServer);
-  runServer(443, secureServer);
-} else {
-  const serverPort: number = process.env.PORT
-    ? parseInt(process.env.PORT)
-    : 9000;
-  runServer(serverPort, regularServer);
-}
+runServer(serverPort, regularServer);

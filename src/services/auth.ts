85cf1a15bee1aad6d51d06de82f59c2ce3c530d2
Bruno / <49591443+bruno-carneiro-da-silva@users.noreply.github.com>
2024-09-22 19:29:24 -0300

    :boom: fix: changing auth logic

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -9,9 +9,9 @@ const REFRESH_TOKEN_SECRET =
 
 export const validadeCredentials = async (
   password: string,
-  email: string
+  emailAdmin: string
 ): Promise<boolean> => {
-  const user = await UsersRepository.findByEmail(email);
+  const user = await UsersRepository.findByEmail(emailAdmin);
   if (!user) {
     return false;
   }
@@ -19,14 +19,14 @@ export const validadeCredentials = async (
   return isPasswordValid;
 };
 
-export const generateToken = (email: string) => {
-  const payload = { email };
+export const generateToken = (emailAdmin: string) => {
+  const payload = { emailAdmin };
   const options = { expiresIn: "15m" };
   return jwt.sign(payload, JWT_SECRET, options);
 };
 
-export const generateRefreshToken = (email: string) => {
-  const payload = { email };
+export const generateRefreshToken = (emailAdmin: string) => {
+  const payload = { emailAdmin };
   const options = { expiresIn: "7d" }; // Refresh token expires in 7 days
   return jwt.sign(payload, REFRESH_TOKEN_SECRET, options);
 };

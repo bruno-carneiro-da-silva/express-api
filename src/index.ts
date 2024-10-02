a1e90da666e40836ab8161bbe12d7566c30c6475
Bruno / <49591443+bruno-carneiro-da-silva@users.noreply.github.com>
2024-10-02 11:40:37 -0300

    :boom: fix: adding helmet for more security and changing the code

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
 import https from "https";
 import fs from "fs";
 import http from "http";
@@ -13,16 +14,38 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(helmet());
+
 app.all("*", requestInterceptor);
 app.use("/admin", adminRoutes);
 app.use("/", loginRoutes);
 
 const runServer = (port: number, server: http.Server) => {
   server.listen(port, () => {
-    console.log(`🚀 is running on http://localhost:${port}`);
+    console.log(`🚀 Server is running on http://localhost:${port}`);
   });
 };
 
 const serverPort: number = process.env.PORT ? parseInt(process.env.PORT) : 9000;
-const regularServer = http.createServer(app);
-runServer(serverPort, regularServer);
+
+if (process.env.NODE_ENV === "production") {
+  const sslKeyPath = process.env.SSL_KEY_PATH;
+  const sslCertPath = process.env.SSL_CERT_PATH;
+
+  if (!sslKeyPath || !sslCertPath) {
+    console.error(
+      "SSL key and certificate paths must be defined in production."
+    );
+    process.exit(1);
+  }
+
+  const httpsOptions = {
+    key: fs.readFileSync(sslKeyPath),
+    cert: fs.readFileSync(sslCertPath),
+  };
+  const secureServer = https.createServer(httpsOptions, app);
+  runServer(serverPort, secureServer);
+} else {
+  const regularServer = http.createServer(app);
+  runServer(serverPort, regularServer);
+}
